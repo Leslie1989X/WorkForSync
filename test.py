ff871e8c37f91f9b6d8830afbc6d7bd068400d89
Leslie1989 <68760695+Leslie1989X@users.noreply.github.com>
2024-10-23 21:53:34 +0800

    save work 20241023

--- test.py
+++ test.py
@@ -1,1 +1,20 @@
 # Hello world
+import sqlite3
+
+conn = sqlite3.connect(r'E:\CodeProject\PycharmProjects\project1\scientificProject\data\database.db')
+c = conn.cursor()
+sql_text_1 = """CREATE TABLE IF NOT EXISTS scores (
+id integer PRIMARY key,
+name text,
+class text,
+gender text);"""
+
+c.execute(sql_text_1)
+for i in range(3, 10):
+    sql_text_2 = f"INSERT INTO scores VALUES ({i},'jxy','1','boy');"
+    c.execute(sql_text_2)
+
+conn.commit()
+
+c.close()
+conn.close()
